fc4f04794e8b6e76dfa8c80a0bf2455ade08193b
jdengoh <spect13317@gmail.com>
2025-08-18 03:21:35 +0800

    fix: removed missing experience component import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/components/Navbar';
 import { Hero } from '@/components/Hero';
 import { About } from '@/components/About';
 import { Projects } from '@/components/Projects';
-import { Experience } from '@/components/Experience';
+// import { Experience } from '@/components/Experience';
 
 export default function Home() {
   return (
